867a1e44b2c07e11d987883e2bb206713e3ee88a
ZhuzumA <Zhuzum@gmail.com>
2022-11-20 00:39:23 +0100

    added some styles and professional informations

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
-import Head from 'next/head';
-import {BsFillMoonStarsFill} from 'react-icons/bs'
+import Head from "next/head";
+import { BsFillMoonStarsFill } from "react-icons/bs";
+import { AiFillGithub, AiFillLinkedin, AiOutlineCodepen } from "react-icons/ai";
+import Image from "next/image";
+import deved from "../public/dev-ed-wave.png";
 
 export default function Home() {
   return (
@@ -10,20 +13,53 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-      <section className='min-h-screen'>
-<nav>
-	<h1>develp</h1>
-	<ul>
-		<li>
-			<BsFillMoonStarsFill />
-			</li>
-		<li><a href="#">Resume</a></li>
-	</ul>
-</nav>
-	  </section>
+      <main className="bg-white px-10">
+        <section className="min-h-screen">
+          <nav className="py-10 mb-12 flex justify-between">
+            <h1 className="text-xl font-burtons">Zhuzum</h1>
+            <ul className="flex items-center">
+              <li>
+                <BsFillMoonStarsFill className="cursor-pointer text-2xl" />
+              </li>
+              <li>
+                <a
+                  className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
+                  href="#"
+                >
+                  Resume
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <div className="text-center p-10">
+            <h2 className="text-5xl text-teal-600 font-medium py-2">
+              Zhuzum Ivanova
+            </h2>
+            <h3 className="text-2xl py-2">Frontend Developer</h3>
+            <p className="text-md py-5 leading-8 text-gray-800">
+              Front-end developer providing services fro programming and design
+              content needs. Join me down below and let's get cracking!
+            </p>
+          </div>
+          <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600">
+            <AiFillLinkedin />
+            <AiFillGithub />
+            <AiOutlineCodepen />
+          </div>
+          <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden">
+            <Image src={deved} layout="fill" objectFit="cover" />
+          </div>
+        </section>
+        <section>
+          <div>
+            <h3 className="text-3xl py-1">Services I offer</h3>
+            <p className="text-md py-2 leading-8 text-gray-800">
+              Since the beginning of my journey as a frontend developer, I have
+              done freelance photographer work
+            </p>
+          </div>
+        </section>
       </main>
-
     </div>
-  )
+  );
 }
