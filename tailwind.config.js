b7653a8df8502ad5142f15c238ed317709c49268
ZhuzumA <Zhuzum@gmail.com>
2022-11-20 23:19:22 +0100

    added responsivness, new styled elements as well as dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+	darkMode: "class",
 	content: [
 	  "./pages/**/*.{js,ts,jsx,tsx}",
 	  "./components/**/*.{js,ts,jsx,tsx}",
@@ -12,4 +13,4 @@ module.exports = {
 	  },
 	},
 	plugins: [],
-  }
+  };
